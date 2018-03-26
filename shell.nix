64ca61b30f66d14fc518c1e0bf350304b2300786
Bassel Mabsout <bmabsout@hotmail.com>
2018-03-26 03:01:34 +0300

    made cabal file and rewrote the bot

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, stdenv }:
+      mkDerivation {
+        pname = "Haskell-Chatbot-Machine-Learning";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base containers ];
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
